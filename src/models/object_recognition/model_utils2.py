a9d8ca3cc6cd361677274cdd1b66d7132f99da23
root <junaid1460@gmail.com>
2018-04-01 14:44:45 +0000

    Fix path trailing mistake

--- src/models/object_recognition/model_utils2.py
+++ src/models/object_recognition/model_utils2.py
@@ -38,7 +38,7 @@ from object_detection.utils import visualization_utils as vis_util
 # In[4]:
 
 # What model to download.
-BASE_FOLDER = '/usr/src/app/weigths'
+BASE_FOLDER = '/usr/src/app/weigths/'
 MODEL_NAME = BASE_FOLDER + 'faster_rcnn_inception_v2_coco_2018_01_28'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
